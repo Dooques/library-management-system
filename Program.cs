6a552869f87781f13b75960f0a19a8da2c258777
Dooques <samuelgdukes@gmail.com>
2026-09-05 18:03:50 +0100

    Library is initialised before the running loop begins

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using library_management.Model;
+using library_management.Services.Database;
 using library_management.Services.IO;
+using Microsoft.EntityFrameworkCore;
+
+using var dbContext = new LibraryContext();
+var library = new Library(dbContext);
 
 var running = true;
 while (running)
