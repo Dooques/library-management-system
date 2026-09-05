fd7d68c62aef258844f1dae556d3423c442fff85
Dooques <samuelgdukes@gmail.com>
2026-09-05 17:00:04 +0100

    Create the book class

--- Model/Book.cs
+++ Model/Book.cs
@@ -2,5 +2,29 @@
 
 public class Book
 {
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public bool IsBorrowed { get; set; }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+        IsBorrowed = false;
+    }
+
+    public void BorrowBook()
+    {
+        IsBorrowed = true;
+    }
     
+    public void ReturnBook()
+    {
+        IsBorrowed = false;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} by {Author} ({IsBorrowed})";
+    }
 }
