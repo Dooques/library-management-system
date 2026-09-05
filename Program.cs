a97f93aa0e3765fb46501a701ab48d91c167c26c
Dooques <samuelgdukes@gmail.com>
2026-09-05 22:14:19 +0100

    Delete and Create a new database on app start.
    Seed the database from Books.json
    
    Check the data is valid with exception handling

--- Program.cs
+++ Program.cs
@@ -1,19 +1,50 @@
+using System.Text.Json;
 using library_management.Model;
 using library_management.Services.Database;
 using library_management.Services.IO;
-using Microsoft.EntityFrameworkCore;
 
 using var dbContext = new LibraryContext();
-var library = new Library(dbContext);
+dbContext.Database.EnsureDeleted();
+dbContext.Database.EnsureCreated();
+
+try
+{
+    var seedBooks = 
+        JsonSerializer.Deserialize<List<Book>>(
+            File.ReadAllText("Resources/Books.json")
+            ) 
+        ?? throw new Exception("No books found in Resources");
+    Console.WriteLine(seedBooks.Count);
+    dbContext.Books.AddRange(seedBooks);
+    dbContext.SaveChanges();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+Console.ReadLine();
+
+var libraryRepo = new LibraryRepository(dbContext);
+var libraryService = new LibraryService(libraryRepo);
 
 var running = true;
 while (running)
 { 
     OutputWriter.WelcomeMessage();
+    
+    Console.Write("    ");
     var userInput = InputReader.ReadInput();
     
     if  (userInput == "exit")
     {
         running = false;
     }
+
+    if (userInput.Contains("add", StringComparison.CurrentCultureIgnoreCase))
+    {
+        libraryService.AddBook();
+    }
+    
 }
