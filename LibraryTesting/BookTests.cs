0bf22e13f807b0d15a63308f80a5713ff9bb4aac
Dooques <samuelgdukes@gmail.com>
2026-09-06 15:41:17 +0100

    Create setup function which instantiates a test book

--- LibraryTesting/BookTests.cs
+++ LibraryTesting/BookTests.cs
@@ -1,10 +1,25 @@
-﻿namespace LibraryTesting;
+﻿using library_management.Model;
+
+namespace LibraryTesting;
 
 public class BookTests
 {
+    private Book _testBook;
+    
     [SetUp]
     public void Setup()
     {
+        _testBook = new Book("test", "john test");
+    }
+
+    [Test]
+    public void CreateBook_ShouldHaveTitleAndAuthor()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(_testBook.Title, Is.EqualTo("test"));
+            Assert.That(_testBook.Author, Is.EqualTo("john test"));
+        });
     }
 
     [Test]
