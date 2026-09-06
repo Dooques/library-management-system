0122e76988e7c3b46c6b0750aca1378573d200ff
Dooques <samuelgdukes@gmail.com>
2026-09-06 17:33:00 +0100

    Begin testing suite for LibraryService.cs

---
+++ LibraryTesting/LibraryRepositoryTesting.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using library_management.Model;
+using library_management.Services.Library;
+using Moq;
+
+namespace LibraryTesting;
+
+public class LibraryServiceTesting
+{
+    private Mock<ILibraryRepository> _mockLibraryRepo;
+    private LibraryService _libraryService;
+
+    private List<Book> _booklist =
+    [
+        new Book("Dune", "Frank Herbert"),
+        new Book("Neuromancer", "William Gibson"),
+        new Book("Foundation", "Isaac Asimov")
+    ];
+    
+    [SetUp]
+
+    public void Setup()
+    {
+        _mockLibraryRepo = new Mock<ILibraryRepository>();
+        _libraryService = new LibraryService(_mockLibraryRepo.Object);
+    }
+
+    [Test]
+    public void ViewBooks_ValidBooks_ShouldReturnAllBooks()
+    {
+        Trace.Listeners.Add(new ConsoleTraceListener());
+        _mockLibraryRepo.Setup(x => 
+            x.GetBooks()).Returns(_booklist);
+        _libraryService.ViewBooks();
+        
+        Assert.That();
+    }
+}
