b580ddc70193484e35c05ffdd98073eea373bf08
Dooques <samuelgdukes@gmail.com>
2026-09-06 14:39:47 +0100

    Begin Unit Testing with Book.cs

---
+++ LibraryTesting/BookTests.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryTesting;
+
+public class BookTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
