8fc87cb260719e23c7d677e60aa7a487c90ec4d4
Dooques <samuelgdukes@gmail.com>
2026-09-06 14:42:37 +0100

    Use nullable types in the LibraryRepository.cs to handle books that don't exist.

--- Model/LibraryRepository.cs
+++ Model/LibraryRepository.cs
@@ -6,9 +6,9 @@ public interface ILibraryRepository
 {
     void AddBook(Book book);
     List<Book> GetBooks();
-    Book SearchBook(string title);
-    Book BorrowBook(string title);
-    Book ReturnBook(string title);
+    Book? SearchBook(string title);
+    Book? BorrowBook(string title);
+    Book? ReturnBook(string title);
 }
 
 public class LibraryRepository(LibraryContext books) : ILibraryRepository
@@ -23,30 +23,29 @@ public class LibraryRepository(LibraryContext books) : ILibraryRepository
         return books.Books.ToList();
     }
 
-    public Book SearchBook(string title)
+    public Book? SearchBook(string title)
     {
         return FindBookInDb(title);
     }
 
-    public Book BorrowBook(string title)
+    public Book? BorrowBook(string title)
     {
         var foundBook = FindBookInDb(title); 
         
-        foundBook.BorrowBook();
+        foundBook?.BorrowBook();
         return foundBook;
     }
 
-    public Book ReturnBook(string title)
+    public Book? ReturnBook(string title)
     {
         var foundBook = FindBookInDb(title);
         
-        foundBook.ReturnBook();
+        foundBook?.ReturnBook();
         return foundBook;
     }
 
-    private Book FindBookInDb(string title)
+    private Book? FindBookInDb(string title)
     {
-        return books.Books.FirstOrDefault(b => b.Title == title) ?? 
-               throw new Exception("Book not found");
+        return books.Books.FirstOrDefault(b => b.Title == title);
     }
 }
